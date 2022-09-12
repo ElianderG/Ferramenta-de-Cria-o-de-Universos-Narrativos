176991bd4e3b34cf2c64b619e5ab12c7d859a894
ElianderG <eliandergomes@gmail.com>
2022-09-12 11:19:38 -0300

    Fisrt Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("test").style.width = "500px";
+}
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("test").style.width = "0px";
+}
