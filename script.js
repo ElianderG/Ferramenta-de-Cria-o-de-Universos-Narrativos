5eef30659dcd28f17e49c154c9bec013ccc2f3b0
ElianderG <eliandergomes@gmail.com>
2022-09-13 11:16:51 -0300

    Começando a ferramenta de calendário

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("sidemainbar").style.width = "30%";
+    document.getElementById('sidemainbar').style.width = "30%";
 }
   
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("sidemainbar").style.width = "0px";
+    document.getElementById('sidemainbar').style.width = "0px";
 }
+
+// Esconder os paragrafos
+function hidepara(){
+    var weekisregular = document.getElementById('weekisregular')
+    var regularweekcb = document.getElementById('regularweek')
+    if (!regularweekcb.checked == true){
+        weekisregular.style.display = "none";
+    } else {
+        weekisregular.style.display = "block"; 
+    }
+}
+
+
+// calculos de dias e semanas para saída
+
+function criarcalendario(){
+    document.getElementById('daysperyear').innerText *= 5+6;
+}
+
